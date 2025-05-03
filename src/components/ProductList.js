07fb779535e9ad2ee1532de028d48c1689e36b0d
SushrutKhajuria <sushrutkhajuria1995@gmail.com>
2025-05-03 15:05:29 +0530

    Product List

---
+++ src/components/ProductList.js
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import { ProductContext, CartContext } from '../context';
+
+const ProductList = () => {
+  const { products } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
+
+  return (
+    <div>
+      {products.map(product => (
+        <div key={product.id} className="product">
+          <h3>{product.name}</h3>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          {Object.entries(product.quantities).map(([size, qty]) => (
+            qty > 0 && (
+              <button key={size} onClick={() => addToCart({ id: product.id, size, price: product.price, name: product.name })}>
+                Buy {size} ({qty} left)
+              </button>
+            )
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
